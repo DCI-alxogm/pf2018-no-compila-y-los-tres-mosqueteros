fe4f5ef77ca96a53a8c63bd893106daf62ad9124
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-12-03 11:31:49 -0600

    .

--- proyectof.c
+++ proyectof.c
@@ -2,28 +2,42 @@
 #include<math.h>
 #include<stdlib.h>
 #include<proyectfmake.h>
-int proyectfmain (){
-FILE *inicial;
-int n=5,i;
-float rt,r1,r2,m,t,h;
-inicial = fopen("inicial.txt","r");
-fscanf(inicial,"%f %f %f\n",&rt,&r1,&r2);
-fscanf(inicial,"%f",&m);
-fscanf(inicial,"%f",&t);
-fscanf(inicial,"%f",&h);
-fclose(inicial);
+
+int proyectfmain()
+{
+
+	FILE *inicial;
+
+	int n=770,i;
+	float rt,r1,r2,m,t,h:
+	float x[n],y[n],z[n],vx[n],vy[n],vz[n],fx[n],fy[n],fz[n];
+	float xp[n],yp[n],zp[n],vxp[n],vyp[n],vzp[n],fxp[n],fyp[n],fzp[n];
+
+
+	inicial = fopen("inicial.txt","r");
+
+	fscanf(inicial,"%f %f %f\n",&rt,&r1,&r2);
+	fscanf(inicial,"%f",&m);
+	fscanf(inicial,"%f",&t);
+	fscanf(inicial,"%f",&h);
+
+	fclose(inicial);
 
 posiciones();
 velocidades();
 
-for (i=0;i<n;i++) {
-x[i+h]=x[i]+h*vx[i]+((1/2)*fx*(pow(h,2)));
-vx[i+h]=vx[i]+((1/2)*(fx[i]+fx[i+h]))*h);
-y[i+h]=y[i]+h*vy[i]+((1/2)*fy*(pow(h,2)));
-vy[i+h]=vy[i]+((1/2)*(fy[i]+fy[i+h]))*h);
-z[i+h]=z[i]+h*vz[i]+((1/2)*fz*(pow(h,2)));
-vz[i+h]=vz[i]+((1/2)*(fz[i]+fz[i+h]))*h);
-}
+for (i=0;i<n;i++)
+	{
+		x[i]=xp[i]+h*vxp[i]+((1/2)*fxp[i]*(pow(h,2)));
+		vx[i]=vxp[i]+((1/2)*(fxp[i]+fx[i]))*h);
+		y[i]=yp[i]+h*vyp[i]+((1/2)*fyp[i]*(pow(h,2)));
+		vy[i]=vyp[i]+((1/2)*(fyp[i]+fy[i]))*h);
+		z[i]=zp[i]+h*vzp[i]+((1/2)*fzp[i]*(pow(h,2)));
+		vz[i]=vzp[i]+((1/2)*(fzp[i]+fz[i]))*h);
+		while(
+)
+
+	}
 
 
 
