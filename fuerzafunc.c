fd1aef2af1b0a05c95058c2091d495e1087db6fe
15luisbeto <42146335+15luisbeto@users.noreply.github.com>
2018-12-08 17:46:37 -0600

    Programa comentado correctamente.

--- fuerzafunc.c
+++ fuerzafunc.c
@@ -1,3 +1,5 @@
+/*FUNCIÓN PARA CALCULAR LAS FUERZAS QUE ACTUAN SOBRE CADA PARTÍCULA EN CADA COMPONENTE A CADA LAPSO DE TIEMPO.*/
+
 #include<proyectfmake.h>
 #include<stdio.h>
 #include<math.h>
@@ -6,7 +8,7 @@
 long double fuerza(long double r,long double x,long double m)
 {
 
-if(r==0) 
+if(r==0) 	// para que la fuerza no sea un valor nulo, si la distancia es igual a cero, igualamos la fuerza a cero. 
 	{
 	return 0;
 	}
@@ -16,3 +18,8 @@ else
 	return (4*3.141596235*3.141596235*x*m*m)/(r*r*r);
 	}
 }
+
+// r = distancia entre las partículas.
+// x = distancia entre las partículas en un un solo componente.
+// m = masa de las partículas.
+
