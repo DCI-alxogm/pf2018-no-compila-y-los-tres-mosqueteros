de35fcb1867fed5ea80b137f7c4f94a20ce337ed
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-12-07 19:54:20 -0600

    Cree un programa que grafica los archivos de texto.

---
+++ grafica.py
@@ -0,0 +1,13 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+for i in range(1,len(sys.argv)):
+    file=sys.argv[i]
+    plt.clf()
+    sp=np.loadtxt(file)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111, projection='3d')
+    ax1.scatter3D(sp[:,0],sp[:,1],sp[:,2])
+    plt.savefig(file+'.png')
