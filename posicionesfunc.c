2f0e77c1e552ccf76a5021854630b7abdf86eff4
15luisbeto <42146335+15luisbeto@users.noreply.github.com>
2018-12-08 16:17:43 -0600

    Programa comentado correctamente.

--- posicionesfunc.c
+++ posicionesfunc.c
@@ -1,4 +1,6 @@
-#include"proyectfmake.h"
+/*FUNCIÓN PARA CALCULAR LAS VELOCIDADES DE CADA PARTICULA EN CADA COMPONENTE A CADA LAPSO DE TIEMPO.*/
+
+#include"proyectfmake.h"	////libreria creada funciones declaradas.
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
@@ -8,3 +10,9 @@ long double posicion(long double xp,long double h,long double vxp,long double fx
 {
 	return xp+(h*vxp)+((1/2)*fxp*(pow(h,2)));
 }
+
+// xp = posición anterior a la interacción.
+// h = el paso de cada interacción.
+// vxp = velocidad anterior a la interacción.
+// fxp = fuerza anterior a la interacción.
+
